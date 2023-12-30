129b136e2b506a174c5ad1f47d5a4d73be1ef61b
RaphaelBirkl1991 <raphael.birkl@gmail.com>
2023-12-30 19:25:30 +0100

    saving note form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,7 @@ import { NgForm } from '@angular/forms';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  
 })
 export class AppComponent implements OnInit {
 
